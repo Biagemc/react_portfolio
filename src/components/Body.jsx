3954a3f41db4f7ae7f0c885499d7025ad31d79f7
biagemc <biagemc@gmail.com>
2020-03-21 16:26:52 -0400

    add header alignment + body structure

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,7 +3,35 @@ import React from "react";
 function Body() {
   return (
     <body>
-      <h1>Body</h1>
+      <div class="top-container">
+        <div class="title-text">
+          <h1>Hey there,</h1>
+          <h2>I'm Moreno...</h2>
+        </div>
+      </div>
+      <div class="middle-container">
+        <div class="profile">
+          <h2>a programmer</h2>
+          <p class="intro">
+            I am a Fullstack Developer, passioned for coffee and developing
+            projects
+          </p>
+        </div>
+
+        <div class="skills">
+          <h2>My Skills.</h2>
+          <div class="skill-row">
+            <h3>Design & Development</h3>
+            <p>
+              I was 10 years old when I started diving into tech. I loved
+              learning new tools on my computer and everything connected. Over
+              time, gained experience developing projects in construction. But,
+              my passion into tech push me back to do what I always loved,
+              developing apps.
+            </p>
+          </div>
+        </div>
+      </div>
     </body>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,19 +21,31 @@ function Header() {
           </button>
 
           <div class="collapse navbar-collapse" id="navbarsExample05">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="">
                   Home <span class="sr-only">(current)</span>
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="">
                   Projects
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="">
+                  Github
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="">
+                  LinkedIn
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="">
                   Contact
                 </a>
               </li>
